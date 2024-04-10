e93c58d25d4ed8d2d4941e4f9f25c22a86450953
sj0320 <112684207+sj0320@users.noreply.github.com>
2024-04-10 19:31:11 +0900

    Add files via upload

---
+++ sol-spring/src/main/java/com/sol/solspring/SolSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sol.solspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolSpringApplication.class, args);
+	}
+
+}
